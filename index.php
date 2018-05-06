9879577da497ec5c73777ab57eb5bd366cb24d81
claytondeasouza/cartolafc <35346022+claytondeasouza@users.noreply.github.com>
2018-05-05 21:10:06 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-102046251-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-102046251-1');
+	</script>
+
+</head>
+<body>
+	
+	
+	<?php
+
+
+	include("menu.html");
+	include("header.html");
+	include("home-teste.html");
+	include("rodape.html");
+	
+	?>
+	
+</body>
+</html>
